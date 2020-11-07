bdc97b1c488161383c2ad83fd0cfd418c63a739d
Tomas <drasutis.tomas@gmail.com>
2020-11-07 20:42:27 +0200

    Initial commit with swagger setup.

---
+++ BaseRateApp.WebApi/BaseRateApp/Startup.cs
@@ -0,0 +1,73 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+
+namespace BaseRateApp
+{
+    public class Startup
+    {
+
+        private readonly string ServiceName = "BaseRateApp";
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            services.AddSwaggerGen(x =>
+            {
+                x.SwaggerDoc("v1", new OpenApiInfo { Title = ServiceName, Version = "v1" });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseSwagger(c =>
+            {
+                c.RouteTemplate = "swagger/{documentName}/swagger.json";
+            });
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("v1/swagger.json", ServiceName + " API V1");
+            });
+
+            app.Use(async (httpContext, next) =>
+            {
+                if (string.IsNullOrEmpty(httpContext.Request.Path) ||
+                    httpContext.Request.Path == "/" ||
+                    httpContext.Request.Path == "/api")
+                {
+                    httpContext.Response.Redirect(httpContext.Request.PathBase + "/swagger");
+                    return;
+                }
+
+                await next();
+            });
+
+            app.UseHttpsRedirection();
+        }
+    }
+}
